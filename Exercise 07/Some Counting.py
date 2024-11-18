f47069447cc97bcdbe80d091bb8e667f6e40ffc8
Omar <omar.abdl.hnn@gmail.com>
2024-11-18 11:36:53 +0400

    Update Some Counting.py

--- Exercise 07/Some Counting.py
+++ Exercise 07/Some Counting.py
@@ -13,27 +13,27 @@ Print all values to the console in each case.
 You may include all loops in a single project
 
 '''
-# Loop that counts up from 0 to 50 in increments of 1
+# Count up from 0 to 50, one step at a time
 print("Counting up from 0 to 50:")
-for i in range(0, 51):  # Starts at 0 and goes up to 50 (inclusive)
-    print(i)  # Print each value of i
+for i in range(0, 51):  # Start at 0 and go up to 50 (inclusive)
+    print(i)  # Print each number as we go
 
-# Loop that counts down from 50 to 0 in decrements of 1
+# Count down from 50 to 0, one step at a time
 print("\nCounting down from 50 to 0:")
-for i in range(50, -1, -1):  # Starts at 50, ends at 0, decreasing by 1 each time
-    print(i)  # Print each value of i
+for i in range(50, -1, -1):  # Start at 50 and go down to 0
+    print(i)  # Print each number as we go
 
-# Loop that counts up from 30 to 50 in increments of 1
+# Count up from 30 to 50, one step at a time
 print("\nCounting up from 30 to 50:")
-for i in range(30, 51):  # Starts at 30 and goes up to 50 (inclusive)
-    print(i)  # Print each value of i
+for i in range(30, 51):  # Start at 30 and go up to 50 (inclusive)
+    print(i)  # Print each number as we go
 
-# Loop that counts down from 50 to 10 in decrements of 2
+# Count down from 50 to 10, two steps at a time
 print("\nCounting down from 50 to 10 in decrements of 2:")
-for i in range(50, 9, -2):  # Starts at 50, ends at 10, decreasing by 2 each time
-    print(i)  # Print each value of i
+for i in range(50, 9, -2):  # Start at 50 and go down to 10, decreasing by 2 each time
+    print(i)  # Print each number as we go
 
-# Loop that counts up from 100 to 200 in increments of 5
+# Count up from 100 to 200, five steps at a time
 print("\nCounting up from 100 to 200 in increments of 5:")
-for i in range(100, 201, 5):  # Starts at 100, goes up to 200 (inclusive), increasing by 5 each time
-    print(i)  # Print each value of i
+for i in range(100, 201, 5):  # Start at 100 and go up to 200 (inclusive), increasing by 5 each time
+    print(i)  # Print each number as we go
