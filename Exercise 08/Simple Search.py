f458fc33160064af0d1bae776f72f3f1c2e6a17c
Omar <omar.abdl.hnn@gmail.com>
2024-11-18 11:24:55 +0400

    Update Simple Search.py

--- Exercise 08/Simple Search.py
+++ Exercise 08/Simple Search.py
@@ -14,12 +14,12 @@ Optional Requirements:
 # Initialize the list of names
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
-# Search for a specific name (basic requirement)
+# Search for a specific name 
 search_name = "Sam"  # Define the name we are looking for
 
 # Check if "Sam" is in the list
 if search_name in names:
-    print(f"{search_name} found in the list!")  # If "Sam" is in the list, print a success message
+    print(f"{search_name} found in the list!")  # If "Sam" is in the list it will print a success message
 else:
     print(f"{search_name} not found in the list.")  # If "Sam" is not in the list, print a failure message
 
